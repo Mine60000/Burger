a948ede13d763187f11bcf9102094ff59f9b84a7
PatrickMckenzie <patrickmckenzie@cmail.carleton.ca>
2025-02-23 17:21:25 -0500

    orders

---
+++ Orderarea_2d.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+var lifted = false
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and not event.pressed:
+		lifted = false
+	if lifted and event is InputEventMouseMotion:
+		position += event.relative
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		lifted = true
